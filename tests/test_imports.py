6d42678194204e158a96a4375530d70cacf196c7
mikegashash <58237401+mikegashash@users.noreply.github.com>
2025-08-09 00:55:13 -0400

    Update test_imports.py

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,7 +1,11 @@
-def test_import_packages():
+def test_imports_ok():
     import services.api.main as api
-    import services.rag.embeddings as emb
-    import services.rag.retriever as rtr
+    import services.rag.index_policies as ip
+    import services.rag.retriever as rt
     import services.rag.reranker as rr
     import services.ocr.pii as pii
+    import services.integrations.models as im
+    import services.integrations.guidewire_adapter as gw
+    import services.integrations.duckcreek_adapter as dc
+    import services.snowflake_io as sf
     assert callable(pii.mask_pii)
