b38e25fe03c2c231651819634f1dcadce1c4f620
mikegashash <58237401+mikegashash@users.noreply.github.com>
2025-08-10 03:26:38 -0400

    Create sitecustomize.py

---
+++ sitecustomize.py
@@ -0,0 +1,5 @@
+# sitecustomize.py  (autoloaded by Python if present on sys.path)
+import os, sys
+root = os.path.dirname(__file__)
+if root not in sys.path:
+    sys.path.insert(0, root)  # ensure repo root comes before site-packages
