314fcd719434adeaef10c025e28a66480decdf8d
mikegashash <58237401+mikegashash@users.noreply.github.com>
2025-08-17 00:49:39 +0000

    integrations: add Duck Creek adapter stub for CI imports

--- claimsight_ai/  integrations/__init__.py
+++ claimsight_ai/  integrations/__init__.py
@@ -1,3 +1,7 @@
 
 # services/integrations/__init__.py
 from . import registry  # or leave empty if you don’t need exports yet
+# claimsight_ai/integrations/__init__.py
+from .models import *  # noqa
+from .guidewire_adapter import *  # noqa
+from .duckcreek_adapter import *  # noqa
